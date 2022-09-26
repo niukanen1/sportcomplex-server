862031264465e3c725de5a75725ac1d7ac277a69
Ivan Niukanen <ivanniu2003@gmail.com>
2022-09-26 22:17:11 +0300

    fixed undefined and null in tickets

--- Schema/Resolvers/Prices/priceListResolver.ts
+++ Schema/Resolvers/Prices/priceListResolver.ts
@@ -5,36 +5,39 @@ import { ObjectId } from "mongodb";
 
 export async function getPriceList() {
 	const cursor = await priceListCollection.find({}).toArray();
-    console.log("first")
+	console.log("first");
 	return cursor;
 }
 
 export async function updatePriceListElementById(id: string, updatedPriceListElement: PriceListElementInput) {
 	let response = "success";
-    console.log("PRoceess")
+
+	const TicketCopy = updatedPriceListElement.tickets ?? [];
+	const newTickets = TicketCopy.filter((el) => el !== undefined && el !== null);
 	try {
 		await priceListCollection.updateOne(
 			{ _id: new ObjectId(id) },
 			{
 				$set: {
-					...updatedPriceListElement,
+					name: updatedPriceListElement.name,
+					tickets: newTickets,
 				},
 			}
 		);
 	} catch (err) {
-        console.log(err)
-        response = "failure";
-        throw new Error("Error updating element " + err);
+		console.log(err);
+		response = "failure";
+		throw new Error("Error updating element " + err);
 	}
-	return await priceListCollection.findOne({_id: new ObjectId(id)});
+	return await priceListCollection.findOne({ _id: new ObjectId(id) });
 }
 
 export async function createNewPriceListElement(newPriceListElement: PriceListElementInput) {
 	const returnObj = { response: {} };
-    console.log("adding new")
+	console.log("adding new");
 	try {
 		const cursor = await priceListCollection.insertOne(newPriceListElement);
-		returnObj.response = await priceListCollection.findOne({_id: cursor.insertedId}) ?? "none";
+		returnObj.response = (await priceListCollection.findOne({ _id: cursor.insertedId })) ?? "none";
 	} catch (err) {
 		console.log(err);
 		throw new Error("Failure creating new price list element " + err);
@@ -42,26 +45,25 @@ export async function createNewPriceListElement(newPriceListElement: PriceListEl
 	return returnObj.response;
 }
 
-
 export async function deletePriceListElementById(_id: string) {
-    let responseStr = "success";  
-    try { 
-        const element = await priceListCollection.findOne({_id: new ObjectId(_id)});
-        await priceListCollection.deleteOne({_id: new ObjectId(_id)}); 
-        if (!element) { 
-            responseStr = "Element Not Found"
-        } 
-    } catch (err) { 
-        responseStr = "error"
-        throw new Error("Failed to delete " + err)
-    }
-    return responseStr
+	let responseStr = "success";
+	try {
+		const element = await priceListCollection.findOne({ _id: new ObjectId(_id) });
+		await priceListCollection.deleteOne({ _id: new ObjectId(_id) });
+		if (!element) {
+			responseStr = "Element Not Found";
+		}
+	} catch (err) {
+		responseStr = "error";
+		throw new Error("Failed to delete " + err);
+	}
+	return responseStr;
 }
 
-export async function getPriceListElementById(id: string) { 
-    const element = await priceListCollection.findOne({ _id: new ObjectId(id)}); 
-    if (!element) { 
-        throw new Error("Element not found...");
-    }
-    return element
+export async function getPriceListElementById(id: string) {
+	const element = await priceListCollection.findOne({ _id: new ObjectId(id) });
+	if (!element) {
+		throw new Error("Element not found...");
+	}
+	return element;
 }
