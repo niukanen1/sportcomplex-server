b6012a151b849b3fc08ab98fe14de5cdf502ed3e
Ivan Niukanen <ivanniu2003@gmail.com>
2022-11-04 13:38:47 +0300

    Update CalendarResolvers.ts

--- Schema/Resolvers/Calendar/CalendarResolvers.ts
+++ Schema/Resolvers/Calendar/CalendarResolvers.ts
@@ -56,7 +56,7 @@ export async function deleteCalendarEvent(_id: string) {
 }
 
 export async function getCalendarEventsByMonth(monthStr: string) { 
-    const arrayOfMonthSortedEvents = await calendarEventsCollection.find({date: {$regex: new RegExp(`${monthStr}-[0-9]{2}`)}}).sort({ date: 1}).toArray();
+    const arrayOfMonthSortedEvents = await calendarEventsCollection.find({date: {$regex: new RegExp(`${monthStr}-[0-9]{2}`)}}).sort({ date: 1, startTime: 1}).toArray();
     return arrayOfMonthSortedEvents;
 }
 
